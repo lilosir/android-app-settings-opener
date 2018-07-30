ac6ae7513a607dea18bf68c13b527da62c9aff51
lilosir <sryoliver@gmail.com>
2018-07-30 15:22:43 -0400

    add gitignore file

--- android/src/main/java/com/lilosir/androidappsettingsopener/AndroidAppSettingsOpenerModule.java
+++ android/src/main/java/com/lilosir/androidappsettingsopener/AndroidAppSettingsOpenerModule.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.net.Uri;
 
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 
 public class AndroidAppSettingsOpenerModule extends ReactContextBaseJavaModule {
 
